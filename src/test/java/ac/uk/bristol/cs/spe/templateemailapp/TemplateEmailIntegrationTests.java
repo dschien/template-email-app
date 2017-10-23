4327a1ceef491e240bd26f0093e939b9ae039b68
Daniel Schien <dschien@gmail.com>
2017-10-23 12:42:37 +0100

    fixed integration test

--- src/test/java/ac/uk/bristol/cs/spe/templateemailapp/TemplateEmailIntegrationTests.java
+++ src/test/java/ac/uk/bristol/cs/spe/templateemailapp/TemplateEmailIntegrationTests.java
@@ -1,5 +1,6 @@
 package ac.uk.bristol.cs.spe.templateemailapp;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,25 +10,31 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@WebMvcTest(TemplateEmailAppApplication.class)
 public class TemplateEmailIntegrationTests {
 
-    @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    TemplateEmailAppApplication templateEmailAppApplication;
+
+    @Before
+    public void setup() {
+        this.mvc = MockMvcBuilders.standaloneSetup(templateEmailAppApplication).build();
+    }
 
     @Test
     public void basicPostRequest() throws Exception {
         mvc.perform(post("/sendemail")
                 .contentType(MediaType.APPLICATION_JSON).content(
-                        "{\"template\": \"The value is {0}\", \"recipientSubstitutions\": {\"daniel.schien@bristol.ac.uk\":[\"1\"]}}"
-                )).andExpect(status().isCreated());
+                        "{\"template\": \"The value is {0}\", \"recipientSubstitutions\": {\"test@me.com\":[\"1\"]}}"
+                )).andExpect(status().isOk());
     }
 
 }
--- src/test/java/ac/uk/bristol/cs/spe/templateemailapp/TemplateEmailServiceTest.java
+++ src/test/java/ac/uk/bristol/cs/spe/templateemailapp/TemplateEmailServiceTest.java
@@ -3,7 +3,6 @@ package ac.uk.bristol.cs.spe.templateemailapp;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -21,7 +20,6 @@ public class TemplateEmailServiceTest {
     @Autowired
     private EmailProvider emailProvider;
 
-    @InjectMocks
     @Autowired
     TemplateEmailService templateEmailService;
 
